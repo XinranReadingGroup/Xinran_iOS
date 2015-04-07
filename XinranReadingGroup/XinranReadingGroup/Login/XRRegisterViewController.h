c8f1f77bdae27c4dddc6007b64a3fa82b8a5e996
晓毒 <zengyun.zy@alibaba-inc.com>
2015-04-07 13:41:51 +0800

    在登录模块努力ing

---
+++ XinranReadingGroup/XinranReadingGroup/Login/XRRegisterViewController.h
@@ -0,0 +1,16 @@
+//
+//  XRRegisterViewController.h
+//  XinranReadingGroup
+//
+//  Created by Xiao Du on 15/4/7.
+//  Copyright (c) 2015年 SnowWolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+ *  注册界面
+ */
+@interface XRRegisterViewController : UIViewController
+
+@end
