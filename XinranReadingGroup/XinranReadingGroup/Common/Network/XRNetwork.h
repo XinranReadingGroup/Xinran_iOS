58f8aa07548ab17db9f8c3ddbd062001d096400c
programmer.du <zengyun.programmer@gmail.com>
2015-04-11 22:08:44 +0800

    feature:(network)网络底层

--- XinranReadingGroup/XinranReadingGroup/Common/Network/XRNetwork.h
+++ XinranReadingGroup/XinranReadingGroup/Common/Network/XRNetwork.h
@@ -7,7 +7,18 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <ZYBlockDefine.h>
 
 @interface XRNetwork : NSObject
+
+@property (nonatomic, strong) NSString *baseURL;
+
++ (XRNetwork *)sharedXRNetwork;
+
++ (XRNetwork *)sharedDoubanNetwork;
+
+- (void)GET:(NSString *)methodName param:(NSDictionary *)param success:(ZYObjectBlock)success failure:(ZYErrorBlock)failure;
+
+- (void)POST:(NSString *)methodName param:(NSDictionary *)param success:(ZYObjectBlock)success failure:(ZYErrorBlock)failure;
 
 @end
--- XinranReadingGroup/XinranReadingGroup/Common/Network/XRNetwork.m
+++ XinranReadingGroup/XinranReadingGroup/Common/Network/XRNetwork.m
@@ -7,9 +7,69 @@
 //
 
 #import "XRNetwork.h"
+#import <ZYCoreDefine.h>
+#import <AFNetworking.h>
 
-static const 
+static NSString * const defaultXRBaseURL = @"http://www.xinrandushuba.com/mobile";
+static NSString * const defaultDoubanBaseURL = @"";
 
 @implementation XRNetwork
+
++ (XRNetwork *)sharedXRNetwork {
+    static XRNetwork *sharedXRNetwork;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedXRNetwork = [[XRNetwork alloc] init];
+        if (sharedXRNetwork) {
+            sharedXRNetwork.baseURL = defaultXRBaseURL;
+        }
+    });
+    
+    return sharedXRNetwork;
+};
+
++ (XRNetwork *)sharedDoubanNetwork {
+    static XRNetwork *sharedDoubanNetwork;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedDoubanNetwork = [[XRNetwork alloc] init];
+        if (sharedDoubanNetwork) {
+            sharedDoubanNetwork.baseURL = defaultDoubanBaseURL;
+        }
+    });
+    
+    return sharedDoubanNetwork;
+};
+
+- (NSString *)urlWithMethodName:(NSString *)methodName {
+    if (![methodName hasPrefix:@"/"]) {
+        methodName = [NSString stringWithFormat:@"/%@",methodName];
+    }
+    return [NSString stringWithFormat:@"%@%@",self.baseURL,methodName];
+}
+
+- (void)GET:(NSString *)methodName param:(NSDictionary *)param success:(ZYObjectBlock)success failure:(ZYErrorBlock)failure {
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager GET:[self urlWithMethodName:methodName] parameters:param success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        if (success) {
+            success(responseObject);
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        failure(error);
+    }];
+}
+
+- (void)POST:(NSString *)methodName param:(NSDictionary *)param success:(ZYObjectBlock)success failure:(ZYErrorBlock)failure {
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager POST:[self urlWithMethodName:methodName] parameters:param success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        if (success) {
+            success(responseObject);
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        if (failure) {
+            failure(error);
+        }
+    }];
+}
 
 @end
