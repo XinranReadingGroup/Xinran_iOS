900ecead8ebbc00f5b0eb820f3ccced5a37f60fd
programmer.du <zengyun.programmer@gmail.com>
2015-04-15 08:59:16 +0800

    feature:(pod)增加了SSKeychain

--- XinranReadingGroup/Pods/Target Support Files/Pods/Pods-environment.h
+++ XinranReadingGroup/Pods/Target Support Files/Pods/Pods-environment.h
@@ -54,6 +54,12 @@
 #define COCOAPODS_VERSION_MINOR_JsonModel 0
 #define COCOAPODS_VERSION_PATCH_JsonModel 2
 
+// SSKeychain
+#define COCOAPODS_POD_AVAILABLE_SSKeychain
+#define COCOAPODS_VERSION_MAJOR_SSKeychain 1
+#define COCOAPODS_VERSION_MINOR_SSKeychain 2
+#define COCOAPODS_VERSION_PATCH_SSKeychain 2
+
 // ZYCoreFramework
 #define COCOAPODS_POD_AVAILABLE_ZYCoreFramework
 #define COCOAPODS_VERSION_MAJOR_ZYCoreFramework 1
