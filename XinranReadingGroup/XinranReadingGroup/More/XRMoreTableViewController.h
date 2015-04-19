dc237d435fd070074f773b499e2c97e679966270
programmer.du <zengyun.programmer@gmail.com>
2015-04-19 17:16:39 +0800

    feature:(more)增加了more界面。并实现了分享功能

---
+++ XinranReadingGroup/XinranReadingGroup/More/XRMoreTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  XRMoreTableViewController.h
+//  XinranReadingGroup
+//
+//  Created by dreamer on 15/4/16.
+//  Copyright (c) 2015年 SnowWolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XRMoreTableViewController : UITableViewController
+
+@end
