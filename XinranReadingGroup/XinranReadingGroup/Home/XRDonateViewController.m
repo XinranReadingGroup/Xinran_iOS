0fd7a61590d669553b88cf13e4eb033a95c228dc
programmer.du <zengyun.programmer@gmail.com>
2015-05-28 22:33:56 +0800

    feature:(捐书界面)增加了一条红色的线方便定位

--- XinranReadingGroup/XinranReadingGroup/Home/XRDonateViewController.m
+++ XinranReadingGroup/XinranReadingGroup/Home/XRDonateViewController.m
@@ -9,6 +9,8 @@
 #import "XRDonateViewController.h"
 #import "XRBookService.h"
 #import "XRDonateResultViewController.h"
+#import <UIView+ZYCore.h>
+#import <ZYCoreDefine.h>
 
 @interface XRDonateViewController ()
 
@@ -26,11 +28,22 @@
 	// Dispose of any resources that can be recreated.
 }
 
+- (void)initUI {
+	[super initUI];
+	UIView *redLine = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 200, 2)];
+	redLine.backgroundColor = RGBACOLOR(255, 0, 0, 0.7);
+	redLine.centerX = self.view.width / 2;
+	redLine.centerY = self.view.height / 2;
+    redLine.tag = 50000;
+	[self.view addSubview:redLine];
+}
+
 - (void)scanFinish:(NSString *)result {
 	[super scanFinish:result];
 	XRDonateResultViewController *donateViewController = [[XRDonateResultViewController alloc] init];
 	donateViewController.ISBN = result;
 	[self.navigationController pushViewController:donateViewController animated:YES];
+	[self startReading];
 }
 
 @end
