d68790e6a9ee60afe05f99863eddeee27940a458
Programmer.Du <zengyun.programmer@gmail.com>
2015-09-26 11:59:35 +0800

    feature:(return book)增加了还书界面

---
+++ XinranReadingGroup/XinranReadingGroup/Home/XRReturnBookResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  XRReturnBookResultViewController.h
+//  XinranReadingGroup
+//
+//  Created by dreamer on 15/8/6.
+//  Copyright (c) 2015年 SnowWolf. All rights reserved.
+//
+
+#import "XRResultBaseViewController.h"
+
+@interface XRReturnBookResultViewController : XRResultBaseViewController
+
+@end
