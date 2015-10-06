6e7be0d7d8813cccb30a82deb7ec888a39997bc0
Programmer.Du <zengyun.programmer@gmail.com>
2015-10-06 13:07:30 +0800

    愉快地使用了新tableview

--- XinranReadingGroup/XinranReadingGroup/More/XRMoreTableViewController.h
+++ XinranReadingGroup/XinranReadingGroup/More/XRMoreTableViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <ZYCoreTableViewController.h>
 
-@interface XRMoreTableViewController : UITableViewController
+@interface XRMoreTableViewController : ZYCoreTableViewController
 
 @end
--- XinranReadingGroup/XinranReadingGroup/More/XRMoreTableViewController.m
+++ XinranReadingGroup/XinranReadingGroup/More/XRMoreTableViewController.m
@@ -6,8 +6,13 @@
 //  Copyright (c) 2015年 SnowWolf. All rights reserved.
 //
 
+#import <ZYCoreFramework/ZYCoreCellInfo.h>
+#import <ZYCoreFramework/ZYTitleCell.h>
+#import <ZYCoreFramework/ZYCoreDefine.h>
 #import "XRMoreTableViewController.h"
 #import "XRShareAssistant.h"
+#import <ZYTitleCell.h>
+#import <ZYCoreCellInfo.h>
 
 @interface XRMoreTableViewController ()
 
@@ -26,42 +31,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    [self setupData];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
-
-#pragma mark - Table view data source
-
-//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-//#warning Potentially incomplete method implementation.
-//    // Return the number of sections.
-//    return 0;
-//}
-//
-//- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-//#warning Incomplete method implementation.
-//    // Return the number of rows in the section.
-//    return 0;
-//}
-
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
-    return cell;
-}
-*/
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     if (indexPath.row == 0) {
@@ -69,48 +44,23 @@
     }
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
+- (void)setupData {
+    ZYCoreCellInfo *shareInfo = [[ZYCoreCellInfo alloc] initWithCellClass:[ZYTitleCell class] withCellHeight:[ZYTitleCell cellHeight] withCellData:@{@"title":LOCALSTRING(@"分享给朋友")} withDidSelectedCallBack:^(UITableView *tableView, ZYCoreTableViewCell *cell, NSIndexPath *indexPath, id cellData) {
+        [self.shareAssistant showShareSheet:self];
+    }];
+    shareInfo.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
+    ZYCoreCellInfo *feedbackInfo = [[ZYCoreCellInfo alloc] initWithCellClass:[ZYTitleCell class] withCellHeight:[ZYTitleCell cellHeight] withCellData:@{@"title":LOCALSTRING(@"意见反馈")} withDidSelectedCallBack:^(UITableView *tableView, ZYCoreTableViewCell *cell, NSIndexPath *indexPath, id cellData) {
 
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
+    }];
+    feedbackInfo.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
 
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
+    ZYCoreCellInfo *aboutInfo = [[ZYCoreCellInfo alloc] initWithCellClass:[ZYTitleCell class] withCellHeight:[ZYTitleCell cellHeight] withCellData:@{@"title":LOCALSTRING(@"关于")} withDidSelectedCallBack:^(UITableView *tableView, ZYCoreTableViewCell *cell, NSIndexPath *indexPath, id cellData) {
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    }];
+    aboutInfo.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    
+    self.tableViewData = @[@[shareInfo,feedbackInfo,aboutInfo]];
 }
-*/
 
 @end
